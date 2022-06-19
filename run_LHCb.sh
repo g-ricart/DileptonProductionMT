5e965c8fdf4d68af2bcf85c8513da77c4a102f24
g-ricart <gabriel.ricart@outlook.fr>
2022-06-19 18:47:57 +0200

    Added dNdQdqTdeta calculations

--- run_LHCb.sh
+++ run_LHCb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./Calculate.exe -etas 0.16 -Q 0 -yQ 2 -Nch 1940 -NSamples 50120000 -area 104 > LHCb_eta016_y=2_Q0.txt
-./Calculate.exe -etas 0.16 -Q 1 -yQ 2 -Nch 1940 -NSamples 50120000 -area 104 > LHCb_eta016_y=2_Q1.txt
-./Calculate.exe -etas 0.32 -Q 0 -yQ 2 -Nch 1940 -NSamples 50120000 -area 104 > LHCb_eta032_y=2_Q0.txt
-./Calculate.exe -etas 0.32 -Q 1 -yQ 2 -Nch 1940 -NSamples 50120000 -area 104 > LHCb_eta032_y=2_Q1.txt
+
+etas=(2.125 2.375 2.625 2.875 3.125 3.375 3.625 3.875 4.125 4.375 4.625 4.875)
+multiplicities=(1747 1678 1637 1577 1536 1509 1433 1351 1256 1213 1176 1142)
+
+for (( i = 0; i < 12; i++ )); do
+    eta="${etas[$i]}"
+    mult="${multiplicities[$i]}"
+    file="output/LHCb_$eta.txt"
+
+    ./Calculate_dqT.exe -etas 0.32 -Q 1 -yQ 0 -Nch $mult -NSamples 50120000 -area 104 > $file
+done
